fe1a6dbf6ab433d0580a01d9917b9f944bb4dd7b
Lukas C. Bossert <lukascbossert@meleager.local>
2021-09-03 11:27:12 +0200

    imported csv-files to database , look at #7

--- hiBC_import.sh
+++ hiBC_import.sh
@@ -2,14 +2,33 @@
 set -euo pipefail
 
 hiBC_NAME="hiBC"
-hiBC_csv="LOCATION.csv
+hiBC_csv="TAX_PHY.csv
+CONDITION.csv
+COUNTRY.csv
+CRYO.csv
+DSMZ.csv
+GENOME.csv
+ISO_ISO.csv
+ISOLATE.csv
+ISOLATION_MEDIA.csv
+ISOLATION.csv
+LOCATION.csv
+MALDI.csv
+MEDIA.csv
+rRNA_SPE_GEN.csv
+rRNA.csv
+SAMPLING.csv
 STAFF.csv
+STORAGE.csv
 TAX_FAM.csv
 TAX_GEN.csv
-TAX_PHY.csv
-TAX_SPE.csv"
-
+TAX_SPE.csv
+TAXONOMY.csv
+TRUE_FALSE.csv
+"
 for hiBC_table in ${hiBC_csv}; do
+	echo "----->>>>>>$(basename ${hiBC_table} .csv) started"
 	importlite -c csv/${hiBC_table} -t $(basename ${hiBC_table} .csv) ${hiBC_NAME}.db
-	#	importlite -c csv/${hiBC_table} -s $(python table_config.py $(basename ${hiBC_table} .csv)) ${hiBC_NAME}.db
+	echo "----->>>>>>$(basename ${hiBC_table} .csv) done"
+	#importlite -c csv/${hiBC_table} -s $(python table_config.py $(basename ${hiBC_table} .csv)) ${hiBC_NAME}.db
 done
