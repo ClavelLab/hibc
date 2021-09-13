7878a3d9d362752b24d7ca9e5020ef05fc023930
Lukas C. Bossert <lukascbossert@meleager.local>
2021-09-13 17:40:56 +0200

    setting pk in rRNA table

--- hiBC_import.sh
+++ hiBC_import.sh
@@ -26,6 +26,10 @@ TAX_SPE.csv
 TAXONOMY.csv
 TRUE_FALSE.csv
 "
+
+# for the tool `importlite` see its webpage
+# https://github.com/KatrinaE/importlite#command-line-arguments
+#
 for hiBC_table in ${hiBC_csv}; do
 	echo "----->>>>>>$(basename ${hiBC_table} .csv) started"
 	importlite -c csv/${hiBC_table} -t $(basename ${hiBC_table} .csv) ${hiBC_NAME}.db
